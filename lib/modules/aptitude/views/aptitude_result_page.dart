f0178ec5f17bdcad828af92b284a6cdaa396154b
ShadaMaitexa <shada.maitexa@gmail.com>
2026-01-22 16:52:28 +0530

    feat: Implement aptitude test result listing and recording pages for students and administrators.

--- lib/modules/aptitude/views/aptitude_result_page.dart
+++ lib/modules/aptitude/views/aptitude_result_page.dart
@@ -130,7 +130,6 @@ class _AptitudeResultListPageState extends State<AptitudeResultListPage> {
                       ),
                     ),
                   ),
-                  ),
                 ),
       ),
     );
